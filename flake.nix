2868638551f5678ab8961558baf1a6513d5ac36f
Reach <reach@42paris.fr>
2026-08-12 01:49:47 +0200

    build(nix): remove flake-utils

--- flake.nix
+++ flake.nix
@@ -3,20 +3,15 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-
-        name = "hello-go-nix";
-        version = "0.1.0";
-      in
-      {
-        devShells.default = pkgs.mkShell {
+  outputs = { nixpkgs, ... }:
+    let
+      forAllSystems = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
           packages = with pkgs; [
             go
             gopls
@@ -26,23 +21,31 @@
             tree-sitter-grammars.tree-sitter-go
           ];
         };
+      });
 
-        packages.default = pkgs.buildGoModule {
-          inherit name version;
-          pname = name;
-          src = ./.;
-          vendorHash = null;
+      packages = forAllSystems (pkgs:
+        let
+          name = "hello-go-nix";
+          version = "0.1.0";
+        in
+        {
+          default = pkgs.buildGoModule {
+            inherit name version;
+            pname = name;
+            src = ./.;
+            vendorHash = null;
 
-          env.CGO_ENABLED = 0;
-          ldflags = [ "-X main.Version=${version}" ];
+            env.CGO_ENABLED = 0;
+            ldflags = [ "-X main.Version=${version}" ];
 
-          meta = {
-            description = "Simple hello app in Go and built with Nix";
-            homepage = "https://github.com/alexandregv/hello-go-nix";
-            license = pkgs.lib.licenses.mit;
-            maintainers = with pkgs.lib.maintainers; [ alexandregv ];
+            meta = {
+              description = "Simple hello app in Go and built with Nix";
+              homepage = "https://github.com/alexandregv/hello-go-nix";
+              license = pkgs.lib.licenses.mit;
+              maintainers = with pkgs.lib.maintainers; [ alexandregv ];
+            };
           };
-        };
-      }
-    );
+        }
+      );
+    };
 }
